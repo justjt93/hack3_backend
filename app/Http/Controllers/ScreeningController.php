6fc1c5c3ce462f5799503bba68ce0c1e1bdfbbff
Martin Hanzlik <hanzlik.martin.92@gmail.com>
2019-10-18 16:08:19 +0200

    second try

--- app/Http/Controllers/ScreeningController.php
+++ app/Http/Controllers/ScreeningController.php
@@ -2,17 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\theater;
 use Carbon\Carbon;
 use Carbon\Traits\Timestamp;
 use Illuminate\Http\Request;
+use DB;
 
 class ScreeningController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        dd(\App\Screening::where('begins_at', ));
-        // $screening = \App\Screening::whereDate('begins_at', '=', Carbon::today()->toDateString());
-        $screening = \App\Screening::whereDate(substr('begins_at',0,10), '=', Carbon::today()->toDateString());
-        return $screening;
+
+        // $query = 'SELECT *
+        //             FROM screenings
+        //             LEFT JOIN movies
+        //              ON screenings.movie_id = movies.id';
+
+        // $results = select($query);
+        // dd($results);
+
+        $slug = $request->input();
+        dd($slug);
+        
+        $screening = DB::table('screenings')
+               ->whereDate('begins_at', date('Y-m-d'))
+               ->where('theater_id', $id)
+               ->pluck('movie_id');
+
+        $movies = DB::table('movies')
+                ->whereIN('id',$screening)
+                ->pluck('name');
+        return $movies;
+        
+
+
+
     }
+
 }
--- app/Theater.php
+++ app/Theater.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Theater extends Model
 {
-    //
+    public function screenings()
+    {
+        return $this->hasMany('App\Screening');
+    }
+
+    public function movies()
+    {
+        return $this->hasOne('App\Movie');
+    }
 }
